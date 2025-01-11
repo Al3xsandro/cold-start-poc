bed5272694f98565ee6259ed39303a3e5e92f6f1
alex <alexs123655@gmail.com>
2025-01-11 11:57:16 -0300

    feat: add embla and config tailwind input

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/**/*.html", "./**/templates/**/*.html"],
+    content: ["./templates/**/*.html", "./**/templates/**/*.html", "./static/js/**/*.js"],
     theme: {
       extend: {},
     },
