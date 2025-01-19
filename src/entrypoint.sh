60368e92f83ac53bc389c3691b5e28d6676c2b03
alex <alexs123655@gmail.com>
2025-01-18 21:43:23 -0300

    chore: set port to uvicorn

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$ENVIRONMENT" = "production" ]; then
     echo "Running in production mode"
     poetry run python manage.py collectstatic --noinput
-    exec poetry run uvicorn coldstart.asgi:application
+    exec poetry run uvicorn coldstart.asgi:application --host 0.0.0.0 --port ${PORT:-8000}
 elif [ "$ENVIRONMENT" = "development" ]; then
     echo "Running in development mode"
     exec poetry run python manage.py runserver 0.0.0.0:8000
